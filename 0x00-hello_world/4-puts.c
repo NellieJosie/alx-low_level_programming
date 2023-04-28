a0b1cd0cc9ed0f2761edfee4070ef44eb43cc9f8
NelllieJosie <nellyjosephine4@gmail.com>
2023-04-28 04:58:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio -h>
+#include <stdio-h>
  /**
   * main - A program that prints a line with puts function
   n
   * Return: 0 (Success)
   */
- main - Entry point
+ main-(entry poin)t
 {
 puts("/"Programming is like building a multilingual puzzle");
  return (o)
