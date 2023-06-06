0645abd950acb331e5251b063dda8352af1caab2
NellieJosie <nellyjosephine4@gmail.com>
2023-06-06 07:33:22 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,12 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
 	int num;
 
 	if (!head || !*head)
 		return (0);
 
 	num = (*head)->n;
-	temp = (*head)->next;
 	free(*head);
 
 	return (num);
